3233174736776d20e2fbf70890893bed62db0957
Davison Mota <davimotagnr@gmail.com>
2023-06-03 16:38:09 -0300

    chore: config coverage jest

--- jest.config.js
+++ jest.config.js
@@ -23,7 +23,7 @@ module.exports = {
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  // coverageDirectory: undefined,
+  coverageDirectory: './coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -121,11 +121,11 @@ module.exports = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  rootDir: './test',
+  rootDir: '.',
 
   // A list of paths to directories that Jest should use to search for files in
   roots: [
-    '<rootDir>'
+    '<rootDir>/test'
   ]
 
   // Allows you to use a custom runner instead of Jest's default test runner
