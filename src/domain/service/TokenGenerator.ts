23e8c3897875c2bef038cb2decedbe151386692a
Davison Mota <davimotagnr@gmail.com>
2023-06-04 15:22:15 -0300

    feat: create verify token

--- src/domain/service/TokenGenerator.ts
+++ src/domain/service/TokenGenerator.ts
@@ -1,13 +1,17 @@
-import { sign } from 'jsonwebtoken'
+import { sign, verify } from 'jsonwebtoken'
 import type User from '../entity/User'
 
 export default class TokenGenerator {
   constructor (private readonly key: string) {}
 
-  generate (user: User, expiresIn: string, issueDate: Date = new Date()): string {
+  generate (user: User, expiresIn: string | number, issueDate: Date = new Date()): string {
     return sign({ username: user.getName(), iat: issueDate.getTime() },
       this.key,
       { expiresIn, noTimestamp: false }
     )
+  }
+
+  validate (token: string): any {
+    return verify(token, this.key)
   }
 }
--- test/unit/service/TokenGenerator.test.ts
+++ test/unit/service/TokenGenerator.test.ts
@@ -16,4 +16,16 @@ describe('TokenGenerator', () => {
     const token = tokenGenerator.generate(user, '1h', new Date('2023-05-04T14:00:00'))
     expect(token).toBe('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImRhdmlzb24iLCJpYXQiOjE2ODMyMTk2MDAwMDAsImV4cCI6MTY4MzIxOTYwMzYwMH0.uVltzQ9DTiyFgj_j6T6kZEsBNgGueF97FxXz_trUvMc')
   })
+
+  test('Deve validar o token', async () => {
+    const tokenGenerator = new TokenGenerator('secret')
+    const user = User.create(
+      new Name('davison'),
+      new Email('davison@gmail.com', new EmailValidatorAdapter()),
+      await Password.create('plainPassword')
+    )
+    const token = tokenGenerator.generate(user, '1h', new Date('2023-05-04T14:00:00'))
+    const payload = tokenGenerator.validate(token)
+    expect(payload.username).toBe('davison')
+  })
 })
