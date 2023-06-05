03cd87f1ab1f8fcf7a3e41fab255b89c8d4c0bd7
Davison Mota <davimotagnr@gmail.com>
2023-06-05 17:38:33 -0300

    test: ensure Bio throw if more then 255 characters

--- test/unit/entity/Bio.test.ts
+++ test/unit/entity/Bio.test.ts
@@ -5,4 +5,9 @@ describe('Bio', () => {
     const bio = new Bio('I love conduit')
     expect(bio.getValue()).toBe('I love conduit')
   })
+
+  test('Não deve criar bio com mais de 255 caracteres', () => {
+    expect(() => new Bio("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.  Lorem Ipsum."))
+      .toThrow(new Error('Invalid bio'))
+  })
 })
