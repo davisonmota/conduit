0f2f835f21e6c219e57c50f0c62ed9342cd744e8
Davison Mota <davisonmota@pop-os.localdomain>
2023-06-23 15:29:13 -0300

    feat: server run if database connection

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,9 @@
 import app from './config/app'
 import env from './config/env'
+import prisma from './database/client'
 
-app.listen(env.port, () => { console.log(`Server running at http://localhost:${env.port}`) })
+prisma.$connect().then(() => {
+  app.listen(env.port, () => { console.log(`Server running at http://localhost:${env.port}`) })
+}).catch(() => {
+  console.log('Server ERROR: could not connect to the database')
+})
