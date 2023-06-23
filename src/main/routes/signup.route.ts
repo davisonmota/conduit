d693d3ac005cc62d5b5be4e46c9d6578609465a7
Davison Mota <davisonmota@pop-os.localdomain>
2023-06-23 15:32:03 -0300

    feat: create factory signup controller

--- src/main/routes/signup.route.ts
+++ src/main/routes/signup.route.ts
@@ -1,19 +1,10 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
-import Login from '../../application/useCase/Login'
-import Signup from '../../application/useCase/Signup'
-import UserRepositoryDatabase from '../../infra/repository/database/UserRepository'
-import SignupController from '../../presentation/controllers/SignupController'
-import { adaptRoute } from '../adapters/expresAdapter'
-import prisma from '../database/client'
+import { adaptRoute } from '../adapters/expressAdapter'
+import { makeSignupController } from '../factories/controllers/SignupControllerFactory'
 
 const signup = Router()
 
-const useRepository = new UserRepositoryDatabase(prisma)
-const signupUseCase = new Signup(useRepository)
-const loginUseCase = new Login(useRepository)
-const signupController = new SignupController(signupUseCase, loginUseCase)
-
-signup.post('/users', adaptRoute(signupController))
+signup.post('/users', adaptRoute(makeSignupController()))
 
 export { signup }
